fb31cc491c2c2f0ca80dd6b2a26fdb3b6096db87
Kamal Raj <64746985+Jellybean6358@users.noreply.github.com>
2026-02-03 14:38:34 +0000

    end stage of search function

--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -3,12 +3,32 @@ from django.db.models import Q
 from .models import Post
 from .forms import PostForm
 
+def search_results(request):
+    query=request.GET.get('q','')
+    print(f"--- DEBUG: Received Query: '{query}' ---")
+    results=[]
+    if query:
+        # Check if user is logged in as per your requirement
+        if request.user.is_authenticated:
+            results = Post.objects.filter(
+                Q(title__icontains=query) | Q(text__icontains=query)
+            )[:5]
+            print(f"--- DEBUG: Found {results.count()} results ---")
+    
+    return render(request, 'posts/search_dropdown.html', {
+        'results': results,
+        'query': query
+    })
+
 def post_list(request):
-    posts=Post.objects.all().order_by('-created_date')
-    query=request.GET.get('q')
+    posts = Post.objects.all().order_by('-created_date')
+    query = request.GET.get('q')
+
     if query and request.user.is_authenticated:
-        posts=posts.filter(Q(title__icontains=query)|Q(text__icontains=query))
-    return render(request,'posts/post_list.html',{'posts':posts,'query':query})
+        # Complex search filter
+        posts = posts.filter(Q(title__icontains=query) | Q(text__icontains=query))
+
+    return render(request, 'posts/post_list.html', {'posts': posts})
 
 def post_detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
